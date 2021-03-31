8a73a533b3e6d986faec922fdb5e7028e3e1c144
Michael Pulido <michaelpulido@Michaels-MBP.maine.rr.com>
2021-03-31 15:55:12 -0400

    routes declared, user controllers cleaned up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :to_dos
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  resources :to_dos, only: [:index, :create, :destroy]
   resources :businesses
-  resources :users
+  resources :users, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
